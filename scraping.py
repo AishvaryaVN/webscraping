a6c6a7eb3924c097fbfad36e7aac4e309e301235
AishvaryaVN <83902412+AishvaryaVN@users.noreply.github.com>
2021-06-30 17:12:51 +0530

    scraping.py

---
+++ scraping.py
@@ -0,0 +1,7 @@
+import requests
+url = 'https://analytics.usa.gov/data/live/realtime.json'
+j = requests.get(url).json()
+print("Number of people visiting a U.S. government website-")
+print("Active Users Right Now:")
+print(j['data'][0]['active_visitors'])
+  
